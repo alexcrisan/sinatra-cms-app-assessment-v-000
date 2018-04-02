39ce45e1c665316b5693454e797a88b6748953ec
Alex Crisan <alexcrisan2005@hotmail.com>
2018-04-02 17:15:54 +0000

    filled in migration files

--- db/migrate/20180402170034_create_beats.rb
+++ db/migrate/20180402170034_create_beats.rb
@@ -1,4 +1,8 @@
 class CreateBeats < ActiveRecord::Migration[5.1]
   def change
+    create_table :beats do |t|
+      t.string :name
+      t.integer :user_id
+    end
   end
 end
--- db/migrate/20180402170111_create_beat_tags.rb
+++ db/migrate/20180402170111_create_beat_tags.rb
@@ -1,4 +1,8 @@
 class CreateBeatTags < ActiveRecord::Migration[5.1]
   def change
+    create_table :beat_tags do |t|
+      t.integer :beat_id
+      t.integer :tag_id
+    end
   end
 end
