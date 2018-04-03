ee9114f409e082fa2ed60bb3d273d8796f640ddd
Alex Crisan <alexcrisan2005@hotmail.com>
2018-04-03 13:58:41 +0000

    finished post signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,32 @@ class UsersController < ApplicationController
     redirect '/' #redirect to index page in application controller
   end
 
-  
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    if params[:username] && params[:email] && params[:password] #if all fields are filled out, proceed to block
+      if !User.find_by(username: params[:username]) #if username doesn't exist, create user and redirect /beats
+        @user = User.create(params)
+        session[:id] = @user.id
+        redirect '/beats'
+      else #if username does exist, redirect to login page.
+        redirect '/users/login'
+      end
+    else #if all fields aren't filled out, reload the signup page.
+      redirect '/signup'
+    end
+  end
+
+  get '/users/login' do
+    erb :'/users/login'
+  end
+
+  get '/users/:username' do #display user's beats
+    @user = User.find_by(username: params[:username])
+    erb :'/users/show'
+  end
+
 
 end
