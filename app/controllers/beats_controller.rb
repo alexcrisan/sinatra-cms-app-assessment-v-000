a77c6c9a143988d14df8dce60a2cd8b3428d47c4
Alex Crisan <alexcrisan2005@hotmail.com>
2018-04-04 17:30:35 +0000

    updated beats controller

--- app/controllers/beats_controller.rb
+++ app/controllers/beats_controller.rb
@@ -2,7 +2,7 @@ class BeatsController < ApplicationController
   get '/beats' do
     if logged_in?
       @user = current_user
-      @beats = Beat.all
+      @beats = current_user.beats
       erb :'/beats/beats'
     else
       redirect '/login'
@@ -11,9 +11,27 @@ class BeatsController < ApplicationController
 
   get '/beats/new' do
     if logged_in?
-      
+      @user = current_user
+      @tags = Tag.all
+      erb :'/beats/create_beat'
     else
       redirect '/login'
+    end
+  end
+
+  post '/beats' do
+    @beat = Beat.create(name: params[:name])
+    if @beat && logged_in?
+      if !params[:tag][:name].empty?
+        @beat.tags << Tag.create(params[:tag])
+      end
+      @beat.save
+      current_user.beats << @beat
+      redirect '/beats'
+    elsif !logged_in?
+      redirect '/login'
+    else
+      redirect '/beats'
     end
   end
 
