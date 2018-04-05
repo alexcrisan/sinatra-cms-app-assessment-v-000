070c70027b7d6d15e7a1b2d6b269e0f167a60d5a
Alex Crisan <alexcrisan2005@hotmail.com>
2018-04-05 03:51:26 +0000

    going through changes

--- app/controllers/beats_controller.rb
+++ app/controllers/beats_controller.rb
@@ -23,7 +23,7 @@ class BeatsController < ApplicationController
     @beat = Beat.create(name: params[:name])
     if @beat && logged_in?
       if !params[:tag][:name].empty?
-        @beat.tags << Tag.create(params[:tag])
+        @beat.tags << Tag.create(params[:tag][:name])
       end
       @beat.save
       current_user.beats << @beat
@@ -36,8 +36,23 @@ class BeatsController < ApplicationController
   end
 
   get '/beats/:slug' do
+    if logged_in?
+      @user = current_user
+      @beat = Beat.find_by_slug(params[:slug])
+      erb :'beats/show_beat'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/beats/:slug/edit' do
     @beat = Beat.find_by_slug(params[:slug])
-    erb :'beats/show_beat'
+    erb :'beats/edit_beat'
+  end
+
+  patch '/beats/:slug' do
+    @beat = Beat.find_by_slug(params[:slug])
+
   end
 
 
--- app/models/beat.rb
+++ app/models/beat.rb
@@ -5,7 +5,7 @@ class Beat < ActiveRecord::Base
 
 
   def slug
-    name.downcase.gsub(" ", "-")
+    self.name.downcase.gsub(" ", "-")
   end
 
   def self.find_by_slug(slug)
